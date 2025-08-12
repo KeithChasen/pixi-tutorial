25e5bc01cd132bbce8600a1279c4372213efcd91
keithchasen <keithchasen89@gmail.com>
2025-08-12 22:04:24 +0200

    init canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-(async () => {})();
+import { Application } from 'pixi.js';
+
+(async () => {
+	const app = new Application();
+
+	await app.init({
+		resizeTo: window,
+	});
+
+	app.canvas.style.position = 'absolute';
+
+	document.body.appendChild(app.canvas);
+})();
