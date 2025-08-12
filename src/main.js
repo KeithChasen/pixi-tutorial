d7e5fb6272441670c9fd5efddb333f51cd11520a
keithchasen <keithchasen89@gmail.com>
2025-08-12 22:13:15 +0200

    add geometric shapes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Application } from 'pixi.js';
+import { Application, Graphics } from 'pixi.js';
 
 (async () => {
 	const app = new Application();
@@ -8,6 +8,21 @@ import { Application } from 'pixi.js';
 	});
 
 	app.canvas.style.position = 'absolute';
+
+	const rectangle = new Graphics()
+		.rect(200, 200, 100, 150)
+		.fill({ color: 0xffea00, alpha: 0.5 })
+		.stroke({
+			width: 8,
+			color: 0x00ff00,
+		});
+
+	const star = new Graphics()
+		.star(1000, 250, 12, 80, 2)
+		.fill({ color: 0xffffff });
+
+	app.stage.addChild(rectangle);
+	app.stage.addChild(star);
 
 	document.body.appendChild(app.canvas);
 })();
